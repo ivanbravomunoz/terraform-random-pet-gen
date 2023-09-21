f03a7a05c847fd8dd9082b89bc5d55aaf555645d
alexbasista <abasista@hashicorp.com>
2023-09-21 14:53:06 -0400

    fix

--- examples/count/main.tf
+++ examples/count/main.tf
@@ -10,15 +10,3 @@ module "pet" {
 
   pet_prefix = data.tfe_outputs.fetch.values.pet_1_name_1
 }
-
-output "pet_1_name" {
-  value = module.pet[0].pet_1_name_1
-}
-
-output "pet_2_name" {
-  value = module.pet[1].pet_1_name_1
-}
-
-output "pet_3_name" {
-  value = module.pet[2].pet_1_name_1
-}
