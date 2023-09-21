50a39a7ba9e1bc413664652cc36d87409914a590
alexbasista <abasista@hashicorp.com>
2023-09-21 14:58:26 -0400

    refactor

--- examples/example-1/main.tf
+++ examples/example-1/main.tf
@@ -2,7 +2,7 @@ module "pet" {
   source  = "app.terraform.io/abasista-tfc/pet-gen/random"
   version = "0.5.0"
 
-  pet_prefix = "main-example"
+  pet_prefix = "example-1"
 }
 
 output "pet_1_name_1" {
--- examples/example-2/main.tf
+++ examples/example-2/main.tf
@@ -5,8 +5,11 @@ data "tfe_outputs" "fetch" {
 
 module "pet" {
   source  = "app.terraform.io/abasista-tfc/pet-gen/random"
-  version = "0.4.0"
-  count   = 3
+  version = "0.5.0"
 
   pet_prefix = data.tfe_outputs.fetch.values.pet_1_name_1
+}
+
+output "pet_name" {
+  value = module.pet.pet_1_name_1
 }
