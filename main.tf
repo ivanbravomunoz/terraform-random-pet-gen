6b3a9aca8b546a9ef1acfbea1033e38c5b30dcb6
alexbasista <abasista@hashicorp.com>
2023-09-21 10:16:12 -0400

    add 3rd pet

--- main.tf
+++ main.tf
@@ -17,3 +17,11 @@ resource "random_pet" "pet_2" {
     always = timestamp()
   }
 }
+
+resource "random_pet" "pet_3" {
+  count = var.pet_count
+
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = "_"
+}
