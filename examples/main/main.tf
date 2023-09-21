e2e6480e7099ad1d10bdff983fb966405018b96e
alexbasista <abasista@hashicorp.com>
2023-09-21 14:44:33 -0400

    change output

--- examples/main/main.tf
+++ examples/main/main.tf
@@ -1,10 +1,10 @@
 module "pet" {
   source  = "app.terraform.io/abasista-tfc/pet-gen/random"
-  version = "0.4.0"
+  version = "0.5.0"
 
   pet_prefix = "main-example"
 }
 
-output "pet_1" {
-  value = module.pet.pet_name_1
+output "pet_1_name_1" {
+  value = module.pet.pet_1_name_1
 }
