0a70b7b50c98e760807fec5687c98c96458dd8f9
alexbasista <abasista@hashicorp.com>
2023-09-22 10:30:05 -0400

    add sens output

--- examples/example-1/main.tf
+++ examples/example-1/main.tf
@@ -1,10 +1,14 @@
 module "pet" {
   source  = "app.terraform.io/abasista-tfc/pet-gen/random"
-  version = "0.5.0"
+  version = "0.6.0"
 
   pet_prefix = "example-1"
 }
 
 output "pet_1_name_1" {
   value = module.pet.pet_1_name_1
+}
+
+output "pet_1_name_1_sensitive" {
+  value = module.pet.pet_1_name_1_sensitive
 }
