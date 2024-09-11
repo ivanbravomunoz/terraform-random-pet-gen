3bb056eb4428899d555a2cf1a12d0126c9cba84d
alexbasista <abasista@hashicorp.com>
2024-09-11 10:31:30 -0400

    normalize outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,3 @@
 output "pet_name_1" {
   value = random_pet.pet_1.id
 }
-
-output "pet_name_2" {
-  value = random_pet.pet_2.*.id
-}
-
-output "pet_name_3" {
-  value = random_pet.pet_3.*.id
-}
-
-output "pet_name_4" {
-  value = random_pet.pet_4.*.id
-}
-
-output "pet_name_5" {
-  value = random_pet.pet_5.*.id
-}
