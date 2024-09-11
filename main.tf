f6be1a5fefe56f3f8f1feca1bd91e5d4c9cf689d
alexbasista <abasista@hashicorp.com>
2024-09-11 10:28:42 -0400

    reduce to one resource

--- main.tf
+++ main.tf
@@ -7,35 +7,3 @@ resource "random_pet" "pet_1" {
     always = timestamp()
   }
 }
-
-resource "random_pet" "pet_2" {
-  count = var.pet_count
-
-  length    = var.pet_length
-  prefix    = var.pet_prefix
-  separator = "-"
-}
-
-resource "random_pet" "pet_3" {
-  count = var.pet_count
-
-  length    = var.pet_length
-  prefix    = var.pet_prefix
-  separator = "_"
-}
-
-resource "random_pet" "pet_4" {
-  count = var.pet_count
-
-  length    = var.pet_length
-  prefix    = var.pet_prefix
-  separator = "_"
-}
-
-resource "random_pet" "pet_5" {
-  count = var.pet_count
-
-  length    = var.pet_length
-  prefix    = var.pet_prefix
-  separator = "_"
-}
