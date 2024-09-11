17a4870b0b552274cda6dcf9db7bd71e15b46fc1
alexbasista <abasista@hashicorp.com>
2024-09-11 10:30:32 -0400

    normalize

--- variables.tf
+++ variables.tf
@@ -3,12 +3,6 @@ variable "pet_prefix" {
   description = "String to prefix random_pet name with."
 }
 
-variable "pet_count" {
-  type        = number
-  description = "How many random_pet resources to create."
-  default     = 1
-}
-
 variable "pet_length" {
   type        = number
   description = "Length in words of random_pet name."
